cca909349d563ebab40ec75d02d2907d2da2b168
Ben Martinez-Bateman <ben@alwaysblank.org>
2017-11-10 11:40:40 -0800

    Added phone methods.

--- src/Phone.php
+++ src/Phone.php
@@ -30,4 +30,40 @@ class Phone
         );
         return $Args;
     }
+
+    /**
+     * Get an HTML element with all the proper telephone
+     * schema tags.
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function phoneSchemaElement(array $args)
+    {
+        $Phone = new static;
+        $numbers = $Phone->rawPhone($args)->get('num');
+        $separator = $Phone->rawPhone($args)->get('separator');
+        return sprintf(
+            '<span itemprop="telephone" content="+%s">%s</span>',
+            join('', \array_diff($numbers, [false, ''])),
+            join($separator, $numbers)
+        );
+    }
+
+    /**
+     * Get a JSON-encoded string containing the telephone
+     * in proper JSON-LD format.
+     *
+     * @param array $args
+     * @return void
+     */
+    public static function phoneSchemaJSON(array $args)
+    {
+        return \json_encode([
+            'telephone' => '+' . join(
+                '', 
+                \array_diff($Phone->rawPhone($args)->get('num'), [false, ''])
+            )
+        ]);
+    }
 }
